c5badaba288924c2db05c6ddef0b6adb8f1bb020
Jennifer Hong <jennifer.hong13@gmail.com>
2018-07-16 23:40:18 +0000

    automatically backed up by learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -3,7 +3,7 @@ def create_an_empty_array
 end
 
 def create_an_array
-  ["Jen", "Jenn", "Jenny", "Jennifer"]
+  names = ["Jen", "Jenn", "Jenny", "Jennifer"]
 end
 
 def add_element_to_end_of_array(array, element)
@@ -15,21 +15,21 @@ def add_element_to_start_of_array(array, element)
 end
 
 def remove_element_from_end_of_array(array)
-  kimberly_name = remove_element_from_end_of_array.pop
+  kimberly_names = remove_element_from_end_of_array.pop
 end
 
 def remove_element_from_start_of_array(array)
-  hone_name = remove_element_from_start_of_array.shift
+  hong_names = remove_element_from_start_of_array.shift
 end
 
 def retrieve_element_from_index(array, index_number)
-
+  names[2]
 end
 
 def retrieve_first_element_from_array(array)
-
+  names[0]
 end
 
 def retrieve_last_element_from_array(array)
-
+  names[3]
 end
