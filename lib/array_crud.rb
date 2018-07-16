922ae3bff1b989d0285b7eb9213685bfbcf10e59
Jennifer Hong <jennifer.hong13@gmail.com>
2018-07-16 23:40:57 +0000

    automatically backed up by learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -7,10 +7,12 @@ def create_an_array
 end
 
 def add_element_to_end_of_array(array, element)
+  names = ["Jen", "Jenn", "Jenny", "Jennifer"]
   add_element_to_end_of_array << "Kimberly"
 end
 
 def add_element_to_start_of_array(array, element)
+  names = ["Jen", "Jenn", "Jenny", "Jennifer"]
   add_element_to_start_of_array.unshift("Hong")
 end
 
